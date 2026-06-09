9947c7876cf427cd48a1cb199ff700c295dfce50
Alejandro <alejandropulido2809@gmail.com>
2026-06-09 09:51:52 +0200

    cambios para que se vea el titulo en el areá de proyectos

---
+++ JS/theme-charger.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themes = [
+        'default',
+        'theme-red',
+        'theme-purple',
+        'theme-black',
+        'theme-white', 
+    ];
+    
+    
+})
